4888bfeb1d2b4428c03b3e85e5437d5510e84ea8
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-05 18:44:25 +0000

    Almost done program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates Volume of Sphere
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radius = parseInt(document.getElementById('radius-mm').value)
+
+  // process
+  const VolumeOfSphere = 4 / 3 * 3.14 (radius * radius * radius)
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is ' + VolumeOfSphere + ' mm'
+}
